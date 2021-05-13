5700ff3e8e11d8d6fc223749ec3eb025ec7fa385
Nick Veltmaat <80262318+nickveltmaat@users.noreply.github.com>
2021-05-13 19:45:04 +0200

    Cleaned up code
    Made the script prettier, no substantive changes

--- comparing_subsets_smillie.R
+++ comparing_subsets_smillie.R
@@ -1,39 +1,39 @@
-#Comparing the smillie subsets
-#Nick Veltmaat
-#3-5-2021
+# Comparing the smillie subsets
+# Nick Veltmaat
+# 3-5-2021
 pacman::p_load(ComplexHeatmap, circlize, NMF, CellChat, Matrix, Seurat)
-#BiocManager::install('UMAP')
+# BiocManager::install('UMAP')
 setwd('~/Desktop/Master Files/Cell_Cell_Project/Cellchat')
 
-#OPTIONAL: load files and compute interactions manually to create cellchat objects: 
+# OPTIONAL: load files and compute interactions manually to create cellchat objects: 
 ###------
-#Load seurat objects / subsets
+# Load seurat objects / subsets
 inflamed_natural <- readRDS('smilliedata/inflamed_natural_composition.rds')
 healthy_natural <- readRDS('smilliedata/natural_composition_subset.rds')
 
-#Convert seurat to Cellchat Objects
+# Convert seurat to Cellchat Objects
 cellchat_inflamed <- createCellChat(object = inflamed_natural, group.by = "ident")
 cellchat_healthy <- createCellChat(object = healthy_natural, group.by = "ident")
 
-#Healthy subset:
+# Healthy subset:
 cellchat_healthy <- setIdent(cellchat_healthy, ident.use = "Cluster") # set "labels" as default cell identity
 levels(cellchat_healthy@idents) # show factor levels of the cell labels
 groupSize_healthy <- as.numeric(table(cellchat_healthy@idents)) # number of cells in each cell group
-#Inflamed subset:
+# Inflamed subset:
 cellchat_inflamed <- setIdent(cellchat_inflamed, ident.use = "Cluster") # set "labels" as default cell identity
 levels(cellchat_inflamed@idents) # show factor levels of the cell labels
 groupSize_inflamed <- as.numeric(table(cellchat_inflamed@idents)) # number of cells in each cell group
 
-#Set database
+# Set database
 CellChatDB <- CellChatDB.human # use CellChatDB.mouse if running on mouse data
-#showDatabaseCategory(CellChatDB)
-#dplyr::glimpse(CellChatDB$interaction)
+# showDatabaseCategory(CellChatDB)
+# dplyr::glimpse(CellChatDB$interaction)
 CellChatDB.use <- subsetDB(CellChatDB, search = "Secreted Signaling") # use Secreted Signaling
-CellChatDB.use <- CellChatDB # simply use the default CellChatDB
+CellChatDB.use <- CellChatDB # or simply use the default CellChatDB
 cellchat_healthy@DB <- CellChatDB.use    #Set DB for Healthy Subset
 cellchat_inflamed@DB <- CellChatDB.use   #Set DB for Inflamed Subset
 
-#Compute Interactions:
+# Compute Interactions:
 #### Healthy subset:
 cellchat_healthy <- subsetData(cellchat_healthy) # subset the expression data of signaling genes for saving computation cost
 future::plan("multiprocess", workers = 4) # do parallel
@@ -47,7 +47,7 @@ cellchat_healthy <- computeCommunProbPathway(cellchat_healthy)
 cellchat_healthy <- aggregateNet(cellchat_healthy)
 cellchat_healthy <- netAnalysis_computeCentrality(cellchat_healthy)
 saveRDS(cellchat_healthy, file = "cellchat_healthy.rds")
-#Compute Interactions:
+# Compute Interactions:
 #### Healthy subset:
 cellchat_inflamed <- subsetData(cellchat_inflamed) # subset the expression data of signaling genes for saving computation cost
 future::plan("multiprocess", workers = 4) # do parallel
@@ -63,8 +63,7 @@ cellchat_inflamed <- netAnalysis_computeCentrality(cellchat_inflamed)
 saveRDS(cellchat_inflamed, file = "cellchat_inflamed.rds")
 ###--------
 
-
-##OR LOAD CELLCHAT OBJECTS FROM RDS:
+## OR LOAD CELLCHAT OBJECTS FROM RDS:
 cellchat_healthy <- readRDS("cellchat_healthy.rds")
 cellchat_inflamed <- readRDS("cellchat_inflamed.rds")
 
@@ -76,11 +75,12 @@ cellchat <- mergeCellChat(object.list,
                           add.names = names(object.list))
 cellchat
 
+##################
+###            ###  
+# VISUALIZATIONS #
+###            ###
 
-###############
-#VISUALIZATIONS
-
-#Compare the total number of interactions and interaction strength
+# Compare the total number of interactions and interaction strength
 gg1 <- compareInteractions(cellchat, 
                            show.legend = F, 
                            group = c(1,2)) 
@@ -90,22 +90,18 @@ gg2 <- compareInteractions(cellchat,
                            measure = "weight")
 gg1 + gg2
 
-#Differentially expressed interactions circle plot
+# Differentially expressed interactions circle plot
 par(mfrow = c(1,2), xpd=TRUE)
 netVisual_diffInteraction(cellchat, weight.scale = T)
 netVisual_diffInteraction(cellchat, weight.scale = T, measure = "weight")
 
-#Heatmap: differential number of interactions or interaction strength
+# Heatmap: differential number of interactions or interaction strength
 gg3 <- netVisual_heatmap(cellchat)
 gg4 <- netVisual_heatmap(cellchat, measure = "weight")
 gg3 + gg4
 
 # Identify signaling networks with larger (or less) difference as well as 
 # signaling groups based on their functional similarity
-#Reset cellchat object
-#object.list <- list(healthy = cellchat_healthy, inflamed = cellchat_inflamed)
-#cellchat <- mergeCellChat(object.list, add.names = names(object.list))
-#
 cellchat <- computeNetSimilarityPairwise(cellchat, type = "functional")
 #> Compute signaling network similarity for datasets 1 2
 cellchat <- netEmbedding(cellchat, type = "functional")
@@ -117,55 +113,62 @@ netVisual_embeddingPairwise(cellchat, type = "functional", label.size = 3.5)
 #> 2D visualization of signaling networks from datasets 1 2
 netVisual_embeddingPairwiseZoomIn(cellchat, type = "functional", nCol = 2)
 #> 2D visualization of signaling networks from datasets 1 2
-
-# Identify signaling groups based on structure similarity
+# Identify signaling groups based on structure similarity using same steps:
 cellchat <- computeNetSimilarityPairwise(cellchat, type = "structural")
-#> Compute signaling network similarity for datasets 1 2
 cellchat <- netEmbedding(cellchat, type = "structural")
-#> Manifold learning of the signaling networks for datasets 1 2
 cellchat <- netClustering(cellchat, type = "structural")
-#> Classification learning of the signaling networks for datasets 1 2
-# Visualization in 2D-space
 netVisual_embeddingPairwise(cellchat, type = "structural", label.size = 3.5)
-#> 2D visualization of signaling networks from datasets 1 2
 netVisual_embeddingPairwiseZoomIn(cellchat, type = "structural", nCol = 2)
-#> 2D visualization of signaling networks from datasets 1 2
 
+#> Compute the distance of signaling networks between datasets 1 2
 rankSimilarity(cellchat, type = "functional")
 rankSimilarity(cellchat, type = "structural")
-#> Compute the distance of signaling networks between datasets 1 2
 
-#Identify and visualize the conserved and context-specific signaling pathways
-#Compare the overall information flow of each signaling pathway
+# Identify and visualize the conserved and context-specific signaling pathways
+# Compare the overall information flow of each signaling pathway
 gg5 <- rankNet(cellchat, mode = "comparison", stacked = T, do.stat = TRUE)
 gg6 <- rankNet(cellchat, mode = "comparison", stacked = F, do.stat = TRUE)
 gg5 + gg6
 
 i = 1
 # combining all the identified signaling pathways from different datasets 
-#Outgoing signals
+# Outgoing signals
 pathway.union <- union(object.list[[i]]@netP$pathways, object.list[[i+1]]@netP$pathways)
-ht1 = netAnalysis_signalingRole_heatmap(object.list[[i]], pattern = "outgoing", signaling = pathway.union, title = names(object.list)[i], width = 5, height = 6)
-ht2 = netAnalysis_signalingRole_heatmap(object.list[[i+1]], pattern = "outgoing", signaling = pathway.union, title = names(object.list)[i+1], width = 5, height = 6)
+ht1 = netAnalysis_signalingRole_heatmap(object.list[[i]], pattern = "outgoing", 
+                                        signaling = pathway.union, 
+                                        title = names(object.list)[i], 
+                                        width = 5, height = 6)
+ht2 = netAnalysis_signalingRole_heatmap(object.list[[i+1]], pattern = "outgoing", 
+                                        signaling = pathway.union, 
+                                        title = names(object.list)[i+1], 
+                                        width = 5, height = 6)
 draw(ht1 + ht2, ht_gap = unit(0.5, "cm"))
-#Incoming Signals
-ht3 = netAnalysis_signalingRole_heatmap(object.list[[i]], pattern = "incoming", signaling = pathway.union, title = names(object.list)[i], width = 5, height = 6, color.heatmap = "GnBu")
-ht4 = netAnalysis_signalingRole_heatmap(object.list[[i+1]], pattern = "incoming", signaling = pathway.union, title = names(object.list)[i+1], width = 5, height = 6, color.heatmap = "GnBu")
+# Incoming Signals
+ht3 = netAnalysis_signalingRole_heatmap(object.list[[i]], pattern = "incoming", 
+                                        signaling = pathway.union, 
+                                        title = names(object.list)[i], width = 5, 
+                                        height = 6, color.heatmap = "GnBu")
+ht4 = netAnalysis_signalingRole_heatmap(object.list[[i+1]], pattern = "incoming", 
+                                        signaling = pathway.union, 
+                                        title = names(object.list)[i+1], width = 5, 
+                                        height = 6, color.heatmap = "GnBu")
 draw(ht3 + ht4, ht_gap = unit(0.5, "cm"))
-#Overall signaling patterns: 
-ht5 = netAnalysis_signalingRole_heatmap(object.list[[i]], pattern = "all", signaling = pathway.union, title = names(object.list)[i], width = 5, height = 6, color.heatmap = "OrRd")
-ht6 = netAnalysis_signalingRole_heatmap(object.list[[i+1]], pattern = "all", signaling = pathway.union, title = names(object.list)[i+1], width = 5, height = 6, color.heatmap = "OrRd")
+# Overall signaling patterns: 
+ht5 = netAnalysis_signalingRole_heatmap(object.list[[i]], pattern = "all", 
+                                        signaling = pathway.union, 
+                                        title = names(object.list)[i], width = 5, 
+                                        height = 6, color.heatmap = "OrRd")
+ht6 = netAnalysis_signalingRole_heatmap(object.list[[i+1]], pattern = "all", 
+                                        signaling = pathway.union, 
+                                        title = names(object.list)[i+1], width = 5, 
+                                        height = 6, color.heatmap = "OrRd")
 draw(ht5 + ht6, ht_gap = unit(0.5, "cm"))
 
-#Part III: Identify the upgulated and down-regulated signaling ligand-receptor pairs
-#Identify dysfunctional signaling by comparing the communication probabities
-#Reset cellchat object
-#object.list <- list(healthy = cellchat_healthy, inflamed = cellchat_inflamed)
-#cellchat <- mergeCellChat(object.list, add.names = names(object.list))
+# Part III: Identify the upgulated and down-regulated signaling ligand-receptor pairs
+# Identify dysfunctional signaling by comparing the communication probabities
 netVisual_bubble(cellchat, sources.use =2, targets.use = c(0:1000),  
                  comparison = c(1, 2), angle.x = 45)
 #> Comparing communications on a merged object
-
 gg7 <- netVisual_bubble(cellchat, sources.use = 5, targets.use = c(0:100),  
                         comparison = c(1, 2), max.dataset = 2, 
                         title.name = "Increased signaling in inflamed", 
@@ -177,16 +180,15 @@ gg8 <- netVisual_bubble(cellchat, sources.use = 5, targets.use = c(0:100),
 #> Comparing communications on a merged object
 gg7 + gg8
 
-
 ###
-#Part IV: Visually compare cell-cell communication using Hierarchy plot, Circle plot or Chord diagram
-#PATHWAYS
+# Part IV: Visually compare cell-cell communication using 
+# Hierarchy plot, Circle plot or Chord diagram
+# PATHWAYS
 ###
 cellchat@netP[["healthy"]][["pathways"]]
 cellchat@netP[["inflamed"]][["pathways"]]
 pathways.show <- c("LAMININ") 
-
-#Circleplot
+# Circleplot
 weight.max <- getMaxWeight(object.list, 
                            slot.name = c("netP"), 
                            attribute = pathways.show) # control the edge weights across different datasets
@@ -197,25 +199,39 @@ for (i in 1:length(object.list)) {
                       edge.width.max = 10, signaling.name = paste(pathways.show, 
                                                                   names(object.list)[i]))
 }
-#Heatmap
+# Heatmap
 par(mfrow = c(1,2), xpd=TRUE)
 ht <- list()
 for (i in 1:length(object.list)) {
-  ht[[i]] <- netVisual_heatmap(object.list[[i]], signaling = pathways.show, color.heatmap = "Reds",title.name = paste(pathways.show, "signaling ",names(object.list)[i]))
+  ht[[i]] <- netVisual_heatmap(object.list[[i]], signaling = pathways.show, 
+                               color.heatmap = "Reds",
+                               title.name = paste(pathways.show, "signaling ",
+                                                  names(object.list)[i]))
 }
 ComplexHeatmap::draw(ht[[1]] + ht[[2]], ht_gap = unit(0.5, "cm"))
 # Chord diagram
 par(mfrow = c(1,2), xpd=TRUE)
 for (i in 1:length(object.list)) {
-  netVisual_aggregate(object.list[[i]], signaling = pathways.show, layout = "chord", signaling.name = paste(pathways.show, names(object.list)[i]))
+  netVisual_aggregate(object.list[[i]], signaling = pathways.show, 
+                      layout = "chord", signaling.name = paste(pathways.show, 
+                                                               names(object.list)[i]))
 }
 
-
-#Part V: Compare the signaling gene expression distribution between different datasets
-#plot the gene expression distribution of signaling genes related to L-R pairs or signaling pathway using a Seurat wrapper function
+# Part V: Compare the signaling gene expression distribution between different datasets
+# plot the gene expression distribution of signaling genes related to L-R pairs or 
+# signaling pathway using a Seurat wrapper function
 ####
-cellchat@meta$datasets = factor(cellchat@meta$datasets, levels = c("healthy", "inflamed")) # set factor level
-plotGeneExpression(cellchat, signaling = pathways.show, split.by = "datasets", colors.ggplot = T)
+cellchat@meta$datasets = factor(cellchat@meta$datasets, 
+                                levels = c("healthy", "inflamed")) # set factor level
+plotGeneExpression(cellchat, signaling = pathways.show, 
+                   split.by = "datasets", colors.ggplot = T)
+####### 
+# End of visualizations
+
+
+
+
+
 
 
 
