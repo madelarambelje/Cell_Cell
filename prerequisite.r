6f12bcaa3da43faeee8827e3f7fcd828a146ed8b
sdey0309 <74510825+sdey0309@users.noreply.github.com>
2021-06-16 14:08:46 +0200

    Update and rename ICELLNET_pkg to prerequisite.r

---
+++ prerequisite.r
@@ -0,0 +1,27 @@
+install.packages("pacman")
+pacman::p_load(devtools, curl, jetset, readxl, psych, GGally, gplots, ggplot2, RColorBrewer, data.table, gridExtra, ggthemes, scales, rlist, Seurat,dplyr, NormalizeData) 
+
+setwd('~/Desktop/Masters/Cell_Cell_Project/ICELLNET')
+
+# Installs Bioconductor dependancies 
+if (!requireNamespace("BiocManager", quietly = TRUE)) 
+  install.packages("BiocManager")
+BiocManager::install(c("BiocGenerics", "org.Hs.eg.db", "hgu133plus2.db", "annotate"))
+
+library(devtools)
+library(BiocGenerics)
+library("org.Hs.eg.db")
+library("hgu133plus2.db")
+library(jetset)
+library(ggplot2)
+library(dplyr)
+library(icellnet)
+library(gridExtra)
+library(Seurat)
+
+#installs ICELLNET package from github
+install_github("soumelis-lab/ICELLNET",ref="master", subdir="icellnet")
+
+
+# creating a dataframe of ICELLNET database
+db=as.data.frame(read.csv(curl::curl(url="https://raw.githubusercontent.com/soumelis-lab/ICELLNET/master/data/ICELLNETdb.tsv"), sep="\t",header = T, check.names=FALSE, stringsAsFactors = FALSE, na.strings = ""))
