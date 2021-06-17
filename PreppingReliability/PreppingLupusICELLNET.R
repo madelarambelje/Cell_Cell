b2e02689f3e372207abd56989c2c11a4a986af3f
madelarambelje <m.a.de.la.rambelje@st.hanze.nl>
2021-06-17 12:19:53 +0200

    Removed PrepPermutation

--- PreppingReliability/PreppingLupusICELLNET.R
+++
@@ -1,8 +0,0 @@
-library("Seurat")
-
-Lupus <- readRDS("TestingPipeline/Lupus_Seurat_SingleCell_Landscape.Rds")
-colnames(Lupus@meta.data)[4] <- "Cluster"
-
-saveRDS(Lupus, "TestingPipeline/Lupus_Seurat_SingleCell_Landscape.Rds")
-
-
