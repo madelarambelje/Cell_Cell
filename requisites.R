d14d273a31365391bebb2f9b8e50b9f9c4e8c862
madelarambelje <75033535+madelarambelje@users.noreply.github.com>
2021-06-17 15:14:28 +0200

    Create requisites.R

---
+++ requisites.R
@@ -0,0 +1,8 @@
+# Install / Load required packages for cellchat analysis
+# Andre de la Rambelje
+
+install.packages('pacman')
+install.packages('BiocManager')
+
+pacman::p_load(igraph, RColorBrewer,matrixStats ,ggplot2, Seurat,scAI)
+
