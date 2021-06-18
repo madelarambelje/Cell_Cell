bbf28013262c4972616550a254241c87a295d83a
madelarambelje <75033535+madelarambelje@users.noreply.github.com>
2021-06-18 15:46:16 +0200

    Update PreppingLupusICELLNET.R

--- PreppingLupusICELLNET.R
+++ PreppingLupusICELLNET.R
@@ -1,6 +1,8 @@
 # Author: Andre de la Rambelje
 
-Lupus <- readRDS("TestingPipeline/Lupus_Seurat_SingleCell_Landscape.Rds")
-colnames(Lupus@meta.data)[4] <- "Cluster"
-
-saveRDS(Lupus, "TestingPipeline/Lupus_Seurat_SingleCell_Landscape.Rds")
+Lupus <- readRDS("seurat_object.rds")
+#Locate the column where cell labels are stored. Change this column name to "Cluster"
+# In this case the column name was found at index four in the meta data.
+colnames(seurat.object@meta.data)[4] <- "Cluster"
+# Save this adjusted seurat.object
+saveRDS(seurat.object, "seurat_object.rds")
