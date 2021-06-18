a2e03b80e1c0c9bc31ba91e73d4beebff106fcc6
Nick Veltmaat <80262318+nickveltmaat@users.noreply.github.com>
2021-06-18 22:06:33 +0200

    Create requisites.R

---
+++ requisites.R
@@ -0,0 +1,11 @@
+# Install / Load required packages for cellchat analysis
+# Nick Veltmaat
+
+install.packages('pacman')
+install.packages('BiocManager')
+
+pacman::p_load(ComplexHeatmap, circlize, NMF, Matrix, Seurat)
+devtools::install_github("sqjin/CellChat")
+pacman::p_load('CellChat')
+
+system("pip3 install umap-learn")
